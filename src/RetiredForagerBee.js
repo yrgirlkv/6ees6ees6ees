f965f6627d2039db8e1f94765e6a1accba7e5b5c
Kavita Poduri <kavi.pdri@gmail.com>
2021-11-15 10:44:37 -0800

    finished assignment

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,5 +1,17 @@
 class RetiredForagerBee extends ForagerBee {
   constructor () {
     super();
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
+  }
+
+  forage () {
+    return 'I am too old, let me play cards instead';
+  }
+
+  gamble (treasure) {
+    this.treasureChest.push(treasure);
   }
 }
