7c655f1aa842c3fc74505251bf4272e4fc45017f
Kavita Poduri <kavi.pdri@gmail.com>
2021-11-15 10:41:00 -0800

    implemented most things

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
-class ForagerBee {
+class ForagerBee extends Bee {
   // TODO..
-};
+  constructor () {
+    super();
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  forage (treasure) {
+    this.treasureChest.push(treasure);
+  }
+}
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,5 @@
-class RetiredForagerBee {
-  // TODO..
-};
+class RetiredForagerBee extends ForagerBee {
+  constructor () {
+    super();
+  }
+}
